cbe7b5a26897d821584f2ec2d6b2accfa7f7629b
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-10 21:10:56 +0200

    ticketing-project-mvc-practice

--- src/main/java/com/cydeo/controller/TaskController.java
+++ src/main/java/com/cydeo/controller/TaskController.java
@@ -6,8 +6,7 @@ import com.cydeo.service.TaskService;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/task")
@@ -40,7 +39,50 @@ public class TaskController {
         return "task/create";
     }
 
+    @PostMapping("/create")
+    public String saveTask(@ModelAttribute("task") TaskDTO task){
 
+        taskService.save(task);
+
+        return "redirect:/task/createPage";
+    }
+
+
+    //delete button
+
+    @GetMapping("/delete/{id}")
+    public String deleteTask(@PathVariable Long id){
+
+        taskService.deleteById(id);
+
+        return "redirect:/task/createPage";
+    }
+
+    // update button
+
+    @GetMapping("/update/{id}")
+    public String editTask(@PathVariable Long id,Model model){
+
+        model.addAttribute("task", taskService.findById(id));
+
+        model.addAttribute("projects" , projectService.readAll());
+
+        model.addAttribute("employees", userService.findEmployees());
+
+        model.addAttribute("tasks", taskService.readAll());
+
+
+        return "/task/update";
+    }
+
+
+    @PostMapping("/update/{id}")
+    public String updateTask(@PathVariable Long id, TaskDTO task){
+
+        taskService.update(task);
+
+        return "redirect:/task/createPage";
+    }
 
 
 
--- src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TaskServiceImpl.java
@@ -1,21 +1,52 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 import com.cydeo.service.TaskService;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TaskServiceImpl extends AbstractMapServiceDB<TaskDTO,Long> implements TaskService {
 
     @Override
     public TaskDTO save(TaskDTO task) {
+
+        if (task.getId() == null){
+            task.setId(UUID.randomUUID().getMostSignificantBits());
+        }
+
+        if(task.getAssignedDate() == null){
+            task.setAssignedDate(LocalDate.now());
+        }
+
+        if (task.getTaskStatus() == null){
+            task.setTaskStatus(Status.OPEN);
+        }
+
+
         return super.save(task.getId(),task);
     }
 
     @Override
     public void update(TaskDTO task) {
+
+
+        if (task.getAssignedDate() == null){
+
+            task.setAssignedDate( findById(task.getId()).getAssignedDate()  );
+
+        }
+
+
+        if (task.getTaskStatus() == null){
+            task.setTaskStatus(  findById(task.getId()).getTaskStatus() );
+        }
+
+
         super.update(task.getId(), task);
     }
 
