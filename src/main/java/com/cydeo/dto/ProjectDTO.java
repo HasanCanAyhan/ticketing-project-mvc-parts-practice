f800301dce3047b35056a34524e3f742b786b478
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-10 16:11:19 +0200

    ticketing-project-mvc-practice

---
+++ src/main/java/com/cydeo/dto/ProjectDTO.java
@@ -0,0 +1,52 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.Status;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor // we use it for project status
+@NoArgsConstructor
+public class ProjectDTO {
+
+    //except String, others are Problem
+
+
+    @NotBlank
+    private String projectName;
+    @NotBlank
+    private String projectCode;
+
+    @NotNull
+    private UserDTO assignedManager;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    @NotBlank
+    private String projectDetail;
+    private Status projectStatus;
+
+
+    private int completeTaskCounts;
+    private int unfinishedTaskCounts;
+
+    public ProjectDTO(String projectName, String projectCode, UserDTO assignedManager, LocalDate startDate, LocalDate endDate, String projectDetail, Status projectStatus) {
+        this.projectName = projectName;
+        this.projectCode = projectCode;
+        this.assignedManager = assignedManager;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.projectDetail = projectDetail;
+        this.projectStatus = projectStatus;
+    }
+}
---
+++ src/main/java/com/cydeo/dto/UserDTO.java
@@ -0,0 +1,50 @@
+package com.cydeo.dto;
+
+import com.cydeo.enums.Gender;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    @NotBlank
+    @Size(max = 15, min = 2)
+    private String firstName;
+
+    @NotBlank
+    @Size(max = 15, min = 2)
+    private String lastName;
+
+    @NotBlank
+    @Email
+    private String userName;
+
+    @NotBlank
+    @Pattern(regexp = "(?=.\\d)(?=.[a-z])(?=.*[A-Z]).{4,}")
+    private String passWord;
+
+
+    private boolean enabled;
+
+    @NotBlank
+    @Pattern(regexp = "^\\d{10}$") // 10 characters
+    private String phone;
+
+    @NotNull
+    private RoleDTO role; // @NotNull bcs it is object, we can use @NotBlank this is for String
+
+    //@NotNull
+    private Gender gender;
+
+
+
+    //@NotNull
+    //private String confirmPassWord;
+
+
+}
